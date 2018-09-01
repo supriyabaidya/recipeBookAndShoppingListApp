4044f2dabb42860afee68742228b54d9ef277ea2
supriyabaidya <33324876+supriyabaidya@users.noreply.github.com>
2018-09-01 20:09:45 +0530

    upto 12 Course Project - Routing (backend 'test server' buttons added and node-server.config.ts file added )

--- src/app/app.node-server.config.ts
+++ src/app/app.node-server.config.ts
@@ -2,6 +2,8 @@
 // serverHost = hostname where node is hosted
 // serverPort = port where node is hosted
 
+// Tip: port 443 is the port which is used for https (secured)
+
 export let config = {
   // serverHost: 'http://localhost',
   serverHost: 'https://my-node-mongodb-test.193b.starter-ca-central-1.openshiftapps.com',
