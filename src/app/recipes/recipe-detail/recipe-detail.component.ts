e2b185049b17a39047cd75913e0a96619949a273
supriyabaidya <33324876+supriyabaidya@users.noreply.github.com>
2018-09-25 18:28:12 +0530

    upto 21 Using Angular Modules & Optimizing Apps

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -37,7 +37,7 @@ export class RecipeDetailComponent implements OnInit {
 
   onDeleteRecipe() {
     this.recipeService.deleteRecipe(this.id);
-    this.router.navigate(['../']);
+    this.router.navigate(['../'], {relativeTo: this.route});
   }
 
 }
